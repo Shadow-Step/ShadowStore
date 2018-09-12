c5220a98464277d0938e1deedcede35e3e615357
ShadowStep <Shadow-Step@mail.ru>
2018-09-12 13:51:02 +0300

    initial commit

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,18 @@ const route = require('express').Router();
 
 route.use((req,res,next)=>{
     if(!req.session.isAdmin){
-        return res.redirect('/login');
+        return res.redirect('/profile/login');
     }
     else{
         next();
     }
 })
 route.get('/',(req,res)=>{
-    res.send('admin');
+    const db = req.app.db.users;
+    db.findOne({email: req.session.email},(err,user)=>{
+        if(user != null){
+            res.render('admin',{user: user});
+        }
+    });
 })
 module.exports = route;
--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,7 @@
 const route = require('express').Router();
 const bcrypt = require('bcryptjs');
+const ObjectID = require('mongodb').ObjectID;
 
-//Profile
-route.get('/',(req,res)=>{
-    
-})
 //Register
 route.get('/register',(req,res)=>{
     res.render('testreg.hbs');
@@ -54,6 +51,7 @@ route.post('/login',(req,res)=>{
             if(bcrypt.compareSync(req.body.pass,user.pass) == true){
                 req.session.email = req.body.email;
                 req.session.isAdmin = true;
+                req.session.logged = true;
                 res.status(200).json({message: 'success'});
             }
             else{
@@ -68,4 +66,29 @@ route.get('/logout',(req,res)=>{
     req.session.email = null;
     res.status(200).json({message: {message: 'logout'}});
 })
+
+//Profile
+route.get('/:id',(req,res)=>{
+    if(!req.session.logged){
+        res.redirect('/profile/login');
+    }
+    const db = req.app.db.users;
+    if(req.params.id.length == 24){
+        let _id = new ObjectID(req.params.id);
+        db.findOne({_id : _id},(err,user)=>{
+            if(user == null){
+                return res.status(400).json({message: 'no such user'});
+            }
+            if(req.session.email === user.email){
+                return res.status(200).json({message: user});
+            }
+            else{
+                return res.status(400).json({message: 'its not you'});
+            }
+        });
+    }
+    else{
+        res.status(400).json({message: 'invalid id'});
+    }
+});
 module.exports = route;
