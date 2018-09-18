5c3f401c681595565e3cbfe9ea3d9909be5aa811
ShadowStep <Shadow-Step@mail.ru>
2018-09-18 15:49:29 +0300

    refactoring

--- routes/profile.js
+++ routes/profile.js
@@ -1,95 +1,91 @@
 const route = require('express').Router();
 const bcrypt = require('bcryptjs');
 const ObjectID = require('mongodb').ObjectID;
+const idIsCorrect = require('../lib/common').idIsCorrect;
+const SafeProfile = require('../lib/model').SafeProfile;
+const Profile = require('../lib/model').Profile;
 
 //Register
 route.get('/register',(req,res)=>{
     res.render('testreg.hbs');
 });
 route.post('/register',(req,res)=>{
+
     const db = req.app.db.users;
-    let user = {email: req.body.email, pass: bcrypt.hashSync(req.body.pass,10)};
-    db.findOne({email: user.email},(err,result)=>{
-        if(err){
-            console.log(err);
-        }
-        if(result == null){
-            db.insertOne(user,(err,insertResult)=>{
-                if(err){
-                    console.log(err);
-                }
-                res.redirect('/admin');
-            })
-        }
-        else{
-            res.status(400).json({message: 'user already exist'});
-        }
+    const user = new Profile(req.body);
+    
+    db.findOne({email: user.email})
+    .then(result => {
+        return result == null ?
+        db.insertOne(user) :
+        Promise.reject({status: 400, message: 'email already exist'})
     })
-})
-/////
+    .then(() => res.status(200).json({message: 'register success'}))
+    .catch(e => console.log(e));
+});
+
 //Login
 route.get('/login',(req,res,)=>{
+
     const db = req.app.db.users;
-    db.count({},(err,count)=>{
-        if(count == 0){
-            res.redirect('/register');
-        }
-        else{
-            res.render('test');
-        }
-    });
+
+    db.countDocuments({})
+    .then(count =>{
+        return count == 0 ?
+        res.redirect('/register') :
+        res.render('test')
+    })
+    .catch(e => console.log(e));
 });
 route.post('/login',(req,res)=>{
+
     const db = req.app.db.users;
-    db.findOne({email:req.body.email},(err,user)=>{
-        if(err){
-            console.log(err);
-        }
-        if(user == null){
-            res.status(400).json({message: 'invalid email'});
+
+    db.findOne({email:req.body.email})
+    .then(user => {
+        return user == null ?
+        Promise.reject({status: 400, message: 'invalid email'}) :
+        bcrypt.compare(req.body.password,user.password)
+    })
+    .then(compareResult => {
+        if(compareResult == true){
+            req.session.email = req.body.email;
+            req.session.isAdmin = true;
+            req.session.logged = true;
+            return res.status(200).json({message: 'login success'});
         }
         else{
-            if(bcrypt.compareSync(req.body.pass,user.pass) == true){
-                req.session.email = req.body.email;
-                req.session.isAdmin = true;
-                req.session.logged = true;
-                res.status(200).json({message: 'success'});
-            }
-            else{
-                res.status(400).json({message: 'invalid password'})
-            }
+            return Promise.reject({status: 400, message: 'invalid password'})
         }
-    });
+    })
+    .catch(e => console.log(e));
 });
 //Logout
 route.get('/logout',(req,res)=>{
     req.session.isAdmin = null;
     req.session.email = null;
-    res.status(200).json({message: {message: 'logout'}});
+    return res.status(200).json({message: {message: 'logout'}});
 })
 
 //Profile
 route.get('/:id',(req,res)=>{
+
     if(!req.session.logged){
         res.redirect('/profile/login');
     }
+    if(!idIsCorrect(req.params.id)){
+        return res.status(400).json({message: 'invalid id'});
+    }
+
     const db = req.app.db.users;
-    if(req.params.id.length == 24){
-        let _id = new ObjectID(req.params.id);
-        db.findOne({_id : _id},(err,user)=>{
-            if(user == null){
-                return res.status(400).json({message: 'no such user'});
-            }
-            if(req.session.email === user.email){
-                return res.status(200).json({message: user});
-            }
-            else{
-                return res.status(400).json({message: 'error'});
-            }
-        });
-    }
-    else{
-        res.status(400).json({message: 'invalid id'});
-    }
+    const _id = new ObjectID(req.params.id);
+
+    db.findOne({_id: _id})
+    .then(profile => {
+        return profile == null ?
+        Promise.reject({status: 400,message: "user doesn't exst"}) :
+        res.status(200).json(new SafeProfile(profile))
+    })
+    .catch(e => console.log(e));
 });
 module.exports = route;
